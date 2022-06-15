6eb6a09af50aa044b936aed6fb93a59ac62d98f9
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-06-15 19:25:04 -0400

    Primeros pasos del programa.

---
+++ KataTemperatureDojoCoding/Range.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTemperatureDojoCoding
+{
+    public class Range
+    {
+        public object text;
+
+        public Range(object text)
+        {
+            this.text = text;
+        }
+
+        string FirstNumber(string x)
+        {
+            string a = x.Substring(2, 1);
+            return a;
+        }
+        string SecondNumber(string x)
+        {
+            string a = x.Substring(4, 1);
+            return a;
+        }
+        string FirstChar(string x)
+        {
+            string a = x.Substring(1, 1);
+            return a;
+        }
+        string SecondChar(string x)
+        {
+            string a = x.Substring(1, 1);
+            return a;
+        }
+
+        string GetAllPoint(string x)
+        {
+            Range text = new Range(x);
+            if(FirstChar(x) == "[")
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
